855d24f7e6a8889b27cb1e6f13a315fd26d93ded
Mark <clashclanacc2602@gmail.com>
2020-05-26 17:26:18 +0300

    maybe not so EOL, but now definitely EOL

--- frd.py
+++ frd.py
@@ -7,16 +7,19 @@ class AudioIO:
     def __init__(self):
         self.p = pyaudio.PyAudio()
         self.byteorder = 'big' # BIG ENDIAN: SIGNIFICANT BYTE FIRST
-        self.speed = 0.6 # 100ms
+        self.speed = 0.29 # 100ms
         self.volume = 1
         self.fs = 44100
-        self.freqhigh = 6500
+        self.freqhigh = 1800
         self.freqlow = 2100
         self.freqStop = 1000
-        self.freqStart = 500
+        self.freqStart = 660
+        self.freqBYTE = 860
+        self.freqEND = 3600
         self.samplehigh = self.CreateSample(self.freqhigh, self.fs, self.speed)
         self.samplelow = self.CreateSample(self.freqlow, self.fs, self.speed)
         self.samplestop = self.CreateSample(self.freqStop, self.fs, self.speed)
+        self.sampleEND = self.CreateSample(self.freqEND, self.fs, self.speed)
         self.streaminput = self.p.open(format=pyaudio.paFloat32,
                 channels=1,
                 rate=self.fs,
@@ -47,11 +50,16 @@ class AudioIO:
         r = open(data, "r")
         bdata = r.read()
         bita = self.tobits(bdata)
+        ctr = 0
         for bit in bita:
+            if ctr == 8:
+                ctr = 0
+                #self.streamout.write(self.volume*self.CreateSample(self.freqBYTE, self.fs, 0.25)) not supported YET
+            ctr += 1
             self.Transmit(bit)
         for _ in range(10):
-            self.streamout.write(self.volume*self.CreateSample(self.freqStop, self.fs, 0.1))
-            self.streamout.write(0*self.CreateSample(self.freqStop, self.fs, self.speed+0.1))
+            self.streamout.write(self.volume*self.CreateSample(self.freqEND, self.fs, self.speed+0.3))
+            self.streamout.write(0*self.CreateSample(self.freqStop, self.fs, 0.1))
     def tobits(self, s):
         ords = (ord(c) for c in s)
         shifts = (7, 6, 5, 4, 3, 2, 1, 0)
--- frd_reciever.py
+++ frd_reciever.py
@@ -9,50 +9,53 @@ class AudioIn:
         return ''.join(chr(int(''.join(x), 2)) for x in zip(*[iter(b)]*8))
     def RealTime(self):
     
-        bytelist = [0]
+        bitlist = [0]
+        ctr = 0
         while True:
-            if len(bytelist) == 220:
-                break
             data = np.fromstring(self.stream.read(1024),dtype=np.int16)
             data = data * np.hanning(len(data)) # smooth the FFT by windowing data
             fft = abs(np.fft.fft(data).real)
             fft = fft[:int(len(fft)/2)]
             freq = np.fft.fftfreq(1024,1.0/44100)
             freq = freq[:int(len(freq)/2)] # keep only first half
-            val = fft[np.where(freq>500)[0][0]]
             freqPeak = freq[np.where(fft==np.max(fft))[0][0]]+1
             freqPeak = round(freqPeak, -2)
             if freqPeak == 0:
                 continue # noise
-            elif freqPeak == 500:
-                print("START")
-                if bytelist[len(bytelist)-1] == 3:
+            elif freqPeak == 700:
+                if bitlist[len(bitlist)-1] == 3:
                     continue # dup
-                bytelist.append(3)
-            elif freqPeak == 6500:
+                print("START")
+                bitlist.append(3)
+            elif freqPeak == 1800: 
+                if bitlist[len(bitlist)-1] == 1:
+                    continue # dupe
                 print("1")
-                if bytelist[len(bytelist)-1] == 1:
-                    continue # dupe
-                bytelist.append(1)
+                bitlist.append(1)
             elif freqPeak == 2100:
+                if bitlist[len(bitlist)-1] == 0:
+                    continue # dupe
                 print("0")
-                if bytelist[len(bytelist)-1] == 0:
-                    continue # dupe
-                bytelist.append(0)
+                bitlist.append(0)
             elif freqPeak == 1000:
-                if bytelist[len(bytelist)-1] == 4:
+                if bitlist[len(bitlist)-1] == 4:
                     continue # dupe
-                bytelist.append(4)
+                bitlist.append(4)
                 print("STOP")
+            elif freqPeak == 3600:
+                if ctr == 1:
+                    print("END TRANSMISSION")
+                    break
+                ctr = 1 # wait for END TRANSMISSION 2 times
             else:
-                #continue
+            #continue
                 print("noise frequency: %d Hz"%freqPeak)
-        bytelist = list(filter((4).__ne__, bytelist))
-        bytelist = list(filter((3).__ne__, bytelist))
-        bytelist = bytelist[1:]
-        print(bytelist)
+        bitlist = list(filter((4).__ne__, bitlist))
+        bitlist = list(filter((3).__ne__, bitlist))
+        bitlist = bitlist[1:]
+        print(bitlist)
         s = ""
-        s = ' '.join([str(elem) for elem in bytelist])
+        s = ' '.join([str(elem) for elem in bitlist])
         print(s)
         s = s.replace(" ", "")
         at = self.bits2a(s)
